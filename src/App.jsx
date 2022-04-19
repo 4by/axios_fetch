23791c50ed581eb90ddbc0cb86ca28b736129ada
idoodi <idoodi@ya.ru>
2022-04-19 18:22:27 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -14,28 +14,33 @@ const axiosGetObj = {
 export default () => {
 
 
-    // PROMISE (fetch)
 
+    // первый асинхронный запрос (fetch(...)) возвращает промис с асинхронным методом json
+    // методом json(), возвразающим результат запроса
+    // соответственно приходится делать второй асинхронный запрос
+
+    // PROMISE (fetch)
     fetch(url)
         .then(resp => resp.json())
         .then(console.log)
 
 
     // ASYNC (fetch)
-
     // (async () => {
     //     console.log(await (await fetch(url)).json())
     // })()
 
 
-    // PROMISE (axios)
+    // асинхронный запрос (axios(...)) возвращает промис со 
+    // свойством data, содержащим результат запроса
 
+
+    // PROMISE (axios)
     // axios(axiosGetObj)
     //     .then(resp => console.log(resp.data))
 
 
     // ASYNC (fetch)
-
     // (async () => {
     //     console.log((await axios(axiosGetObj)).data)
     // })()
